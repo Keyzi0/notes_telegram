cbda6e8dd3ff55cf1bc5026e56788320d57a8f99
degtyarev@rt-bpm.ru <degtyarev@rt-bpm.ru>
2022-05-30 21:00:45 +0400

    add bot

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/Keyzi0/notes_telegram
+
+go 1.18
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/gookit/config/v2 v2.1.1
+)
+
+require (
+	github.com/gookit/goutil v0.5.2 // indirect
+	github.com/imdario/mergo v0.3.13 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/Keyzi0/notes_telegram/models"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/gookit/config/v2"
+	"github.com/gookit/config/v2/yaml"
+)
+
+func main() {
+	cfg := getConfig()
+	bot, err := tgbotapi.NewBotAPI(cfg.BotToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
+}
+
+func getConfig() models.Config {
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
+
+	config.WithOptions(config.ParseEnv)
+
+	// add driver for support yaml content
+	config.AddDriver(yaml.Driver)
+
+	if err = config.LoadFiles("./config.yml"); err != nil {
+		panic(err)
+	}
+	var configs models.Config
+	config.BindStruct("bot", &configs)
+	fmt.Println(fmt.Sprintf("config: %v", configs))
+	return configs
+}
